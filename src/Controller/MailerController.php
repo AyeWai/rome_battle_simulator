c0b6107cb4ced2c8baef7e20e3d96d470afd8ffa
Chris <cs.simon@live.fr>
2023-09-06 12:37:48 +0200

    Add mail controller and contact mail template

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
+class MailerController extends AbstractController
+{
+    #[Route('/email')]
+    public function sendEmail(MailerInterface $mailer, Request $request): void
+    {
+        $email = (new TemplatedEmail())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Roman Battle Simulator Feedback')
+            ->htmlTemplate('contact/index.html.twig');
+
+        $mailer->send($email);
+    }
+}
