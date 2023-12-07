a0f4c26e120646b7be61970bd0f04edca3524afe
Chris <cs.simon@live.fr>
2023-12-07 15:47:19 +0100

    Centuria - Mailer Controler and LanguageSwitcher

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -12,11 +12,11 @@ use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
 class MailerController extends AbstractController
 {
-    #[Route('/email')]
+    #[Route('/email', name: 'app_email')]
     public function sendEmail(MailerInterface $mailer, Request $request): void
     {
         $email = (new TemplatedEmail())
-            ->from('hello@example.com')
+            ->from('framework.mailer.sender')
             ->to('you@example.com')
             //->cc('cc@example.com')
             //->bcc('bcc@example.com')
--- src/Service/LanguageSwitcher.php
+++ src/Service/LanguageSwitcher.php
@@ -12,9 +12,6 @@ class LanguageSwitcher
 
     public function switchLocaleToFrench(): void
     {
-        // you can get the current application locale like this:
-        $currentLocale = $this->localeSwitcher->getLocale();
-
         // you can set the locale for the entire application like this:
         // (from now on, the application will use 'fr' (French) as the
         // locale; including the default locale used to translate Twig templates)
@@ -27,5 +24,11 @@ class LanguageSwitcher
         // reset the current locale of your application to the configured default locale
         // in config/packages/translation.yaml, by option 'default_locale'
         $this->localeSwitcher->reset();
+    }
+
+    public function getLocale()
+    {
+        // you can get the current application locale like this:
+        return $this->localeSwitcher->getLocale();
     }
 }
